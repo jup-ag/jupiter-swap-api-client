8477ca4cf1ff889852d52e9b71057d19f3a65e68
Arrowana <Arrowana@users.noreply.github.com>
2023-08-28 13:22:15 +1000

    downgrade to 1.14, add sign + send

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Jupiter api rust example
 
-We can autogenerate a client with openapi-gen and the openapi schema but the structs are not directly consumable solana sdk or rust types. As a result we would need
+We can autogenerate a client with openapi-gen and the openapi schema but the structs cannot be directly consumed as solana sdk or appropirate rust types.
+
+The crate `jupiter-swap-api` exposes the API types to allow
+
+If you must use solana 1.16 crates, there is a work in progress to relax tokio pinned too low conflicting with many other crates https://github.com/solana-labs/solana/pull/32943
--- example/src/main.rs
+++ example/src/main.rs
@@ -6,8 +6,9 @@ use jupiter_swap_api::{
     transaction_config::TransactionConfig,
 };
 use reqwest::Client;
-use solana_sdk::pubkey;
-use solana_sdk::pubkey::Pubkey;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{pubkey, transaction::VersionedTransaction};
+use solana_sdk::{pubkey::Pubkey, signature::NullSigner};
 use tokio;
 
 const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
@@ -18,7 +19,7 @@ pub const TEST_WALLET: Pubkey = pubkey!("2AQdpHJ2JpcEgPiATUXjQxA8QmafFegfQwSLWSp
 #[tokio::main]
 async fn main() {
     let quote_api_base_url =
-        env::var("API_BASE_URL").unwrap_or("https://quote-api.jup.ag/v5".into());
+        env::var("API_BASE_URL").unwrap_or("https://quote-api.jup.ag/v6".into());
     println!("Using base url: {}", quote_api_base_url);
 
     let quote_request = QuoteRequest {
@@ -32,6 +33,7 @@ async fn main() {
     println!("The quote query is {}", query);
 
     let client = Client::new();
+    // GET /quote
     let quote_response = client
         .get(format!("{quote_api_base_url}/quote?{query}"))
         .send()
@@ -42,6 +44,7 @@ async fn main() {
         .unwrap();
     println!("{quote_response:#?}");
 
+    // POST /swap
     let swap_response = client
         .post(format!("{quote_api_base_url}/swap"))
         .json(&SwapRequest {
@@ -56,4 +59,21 @@ async fn main() {
         .await
         .unwrap();
     println!("Raw tx len: {}", swap_response.swap_transaction.len());
+
+    let versioned_transaction: VersionedTransaction =
+        bincode::deserialize(&swap_response.swap_transaction).unwrap();
+
+    // Replace with a keypair or other struct implementing signer
+    let null_signer = NullSigner::new(&TEST_WALLET);
+    let signed_versioned_transaction =
+        VersionedTransaction::try_new(versioned_transaction.message, &[&null_signer]).unwrap();
+
+    // send with rpc client...
+    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com".into());
+
+    // This will fail with "Transaction signature verification failure" as we did not really sign
+    rpc_client
+        .send_and_confirm_transaction(&signed_versioned_transaction)
+        .await
+        .unwrap();
 }
