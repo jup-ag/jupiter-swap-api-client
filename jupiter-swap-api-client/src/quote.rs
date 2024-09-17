9fd4adb92a279affc478eb18ce5d544690bbea30
elvisyong <elvisyong@gmail.com>
2024-09-18 00:48:42 +0800

    feat: update quote request fields with later versions

--- jupiter-swap-api-client/src/quote.rs
+++ jupiter-swap-api-client/src/quote.rs
@@ -58,11 +58,26 @@ pub struct QuoteRequest {
     pub input_mint: Pubkey,
     #[serde(with = "field_as_string")]
     pub output_mint: Pubkey,
+    /// The amount to swap, have to factor in the token decimals.
     #[serde(with = "field_as_string")]
     pub amount: u64,
+    /// (ExactIn or ExactOut) Defaults to ExactIn.
+    /// ExactOut is for supporting use cases where you need an exact token amount, like payments.
+    /// In this case the slippage is on the input token.
     pub swap_mode: Option<SwapMode>,
     /// Allowed slippage in basis points
     pub slippage_bps: u16,
+    /// Default is false.
+    /// By setting this to true, our API will suggest smart slippage info that you can use.
+    /// slippageBps is what we suggest you to use. Additionally, you should check out max_auto_slippage_bps and auto_slippage_collision_usd_value.
+    pub auto_slippage: Option<bool>,
+    /// The max amount of slippage in basis points that you are willing to accept for auto slippage.
+    pub max_auto_slippage_bps: Option<u16>,
+    pub compute_auto_slippage: bool,
+    /// The max amount of USD value that you are willing to accept for auto slippage.
+    pub auto_slippage_collision_usd_value: Option<u32>,
+    /// Quote with a greater amount to find the route to minimize slippage
+    pub minimize_slippage: Option<bool>,
     /// Platform fee in basis points
     pub platform_fee_bps: Option<u8>,
     pub dexes: Option<Vec<String>>,
@@ -71,12 +86,17 @@ pub struct QuoteRequest {
     pub only_direct_routes: Option<bool>,
     /// Quote fit into legacy transaction
     pub as_legacy_transaction: Option<bool>,
+    /// Restrict intermediate tokens to a top token set that has stable liquidity.
+    /// This will help to ease potential high slippage error rate when swapping with minimal impact on pricing.
+    pub restrict_intermediate_tokens: Option<bool>,
     /// Find a route given a maximum number of accounts involved,
     /// this might dangerously limit routing ending up giving a bad price.
     /// The max is an estimation and not the exact count
     pub max_accounts: Option<usize>,
     // Quote type to be used for routing, switches the algorithm
     pub quote_type: Option<String>,
+    // enable only full liquid markets as intermediate tokens
+    pub prefer_liquid_dexes: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -103,6 +123,10 @@ pub struct QuoteResponse {
     pub other_amount_threshold: u64,
     pub swap_mode: SwapMode,
     pub slippage_bps: u16,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub computed_auto_slippage: Option<u16>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub uses_quote_minimizing_slippage: Option<bool>,
     pub platform_fee: Option<PlatformFee>,
     pub price_impact_pct: String,
     pub route_plan: RoutePlanWithMetadata,
