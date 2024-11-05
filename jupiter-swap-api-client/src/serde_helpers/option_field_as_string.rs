090b59cd951e307fd10cd4c30c9048d56e15ca7b
elvisyong <elvisyong@gmail.com>
2024-11-05 20:59:51 +0800

    feat: implement deserialize for option_field_as_string

--- jupiter-swap-api-client/src/serde_helpers/option_field_as_string.rs
+++ jupiter-swap-api-client/src/serde_helpers/option_field_as_string.rs
@@ -1,4 +1,7 @@
-use {serde::Serialize, serde::Serializer};
+use {
+    serde::{de, Deserialize, Deserializer, Serialize, Serializer},
+    std::str::FromStr,
+};
 
 pub fn serialize<T, S>(t: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
 where
@@ -9,5 +12,21 @@ where
         t.to_string().serialize(serializer)
     } else {
         serializer.serialize_none()
+    }
+}
+
+pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    T: FromStr,
+    D: Deserializer<'de>,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
+    let opt: Option<String> = Option::deserialize(deserializer)?;
+    match opt {
+        Some(s) => s
+            .parse()
+            .map(Some)
+            .map_err(|e| de::Error::custom(format!("Parse error: {:?}", e))),
+        None => Ok(None),
     }
 }
